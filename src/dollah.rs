93aba94f249dfc92034b69eb1b1dea0535569f69
Mythbusters123 <atr10605@hotmail.com>
2022-01-21 13:22:54 -0500

    Code cleanup

--- src/dollah.rs
+++ src/dollah.rs
@@ -8,10 +8,17 @@ fn main() {
         exit(1);
     }
 
-    let mut program: Child = Command::new(args.iter().nth(1).unwrap_or(&"echo \"No Program supplied\"".to_owned()))
-        .args(args.iter().skip(2)).spawn().expect("Could not start process.");
+    let mut program: Child = Command::new(args
+            .iter()
+            .nth(1)
+            .unwrap_or(&"echo \"No Program specified".to_owned()))
+            .args(args
+               .iter()
+               .skip(2))
+        .spawn()
+        .expect("Could not start process.");
 
-    let ecode = program.wait().expect("Failed to wait on process.");
+    let ecode = program.wait().expect("Failed to wait on process.").code();
 
-    exit(ecode.code().expect("Error getting code."))
+    exit(ecode.expect("Error getting code"))
 }
